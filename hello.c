b43c34485c8ab3fecc9d8e1943a51853dbfc5378
SHREYA <shreyaarush2015@gmail.com>
2025-08-24 13:56:50 +0530

    little changes

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+  int num;
+  char character;
+  printf("Enter a number= ");
+  scanf("%d",&num);
+  printf("Enter a character = ");
+  scanf(" %c",&character);
+}
