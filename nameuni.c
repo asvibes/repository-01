c04209ce906aa823d6a143b421c47f8d8c9a4fb5
SHREYA <shreyaarush2015@gmail.com>
2025-08-27 20:01:35 +0530

    code of name uni course

---
+++ nameuni.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  char name,uni,course;
+  scanf("%s",&name);
+  scanf("%s",&uni);
+  scanf("%s",&course);
+  printf("name is ",name,"\n");
+  printf("University name is ",uni,"\n");
+  printf("Course is ",course,"\n");
+  return 0;
+}
+  
