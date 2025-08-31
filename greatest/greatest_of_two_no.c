ff5cb298947853204576400323f90814a8f84d9f
SHREYA <shreyaarush2015@gmail.com>
2025-08-31 19:59:26 +0530

    greatest of two no.

---
+++ greatest/greatest_of_two_no.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int a,b;
+  
+  scanf("%d",&a);
+  
+  scanf("%d",&b);
+  if (a>b)
+  {
+    printf( "%d number is greater ", a);
+  }
+  else{
+    printf("%dnumber is greater ",b);
+  }
+  return 0;
