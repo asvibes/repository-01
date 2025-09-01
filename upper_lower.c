66e99119cd2352d61716e86c6a6913fd5c5ea7bd
SHREYA <shreyaarush2015@gmail.com>
2025-09-01 10:37:27 +0530

    upper case & lower case

---
+++ upper_lower.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    char alphabet;
+    scanf("%c",&alphabet);
+    if 
+    (alphabet== 'a' ||
+    alphabet== 'b' ||
+    alphabet== 'c' ||
+    alphabet== 'd' ||
+    alphabet== 'e' ||
+    alphabet== 'f' ||
+    alphabet== 'g' ||
+    alphabet== 'h' ||
+    alphabet== 'j' ||
+    alphabet=='i'||
+    alphabet== 'k' ||
+    alphabet== 'l' ||alphabet== 'm' ||alphabet== 'n' ||alphabet== 'p' ||alphabet== 'q' ||
+    alphabet=='o' || alphabet== 'r' ||alphabet== 's' ||alphabet== 't' ||alphabet== 'v' ||
+    alphabet=='u' || alphabet== 'w' ||alphabet== 'x' ||alphabet== 'y' ||alphabet== 'z' )
+    {
+    printf("is lower case");}
+    else{
+      printf("is uppercase");}
+}
